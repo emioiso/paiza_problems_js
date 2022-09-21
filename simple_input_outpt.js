8af46430ca9f8669eabab5c1487f3b2e198b8a47
emi_oiso <106772442+emioiso@users.noreply.github.com>
2022-09-22 03:36:48 +0900

    Create simple_input_outpt.js

---
+++ simple_input_outpt.js
@@ -0,0 +1,20 @@
+/*
+単純な入出力 (paizaランク D 相当)
+【期待する出力】
+paiza
+*/
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  console.log(lines[0]); // paiza
+});
